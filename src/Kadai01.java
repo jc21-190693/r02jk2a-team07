a013e181120475a5952853121a9c0596dcbabaf9
190693 <190693@e0505-27.JC-21.local>
2020-12-01 15:50:42 +0900

    課題１終わりました

--- src/Kadai01.java
+++ src/Kadai01.java
@@ -1,6 +1,6 @@
 
 public class Kadai01 {
-	
+
 	/**
 	 * 課題01
 	 * 文字列の左右の文字を反転して返すメソッド
@@ -9,6 +9,15 @@ public class Kadai01 {
 	 */
 	String ReverseString(String s) {
 		//TODO プログラムをここに作成する
-		return null;
+		if(s==null) {
+		return s;
+		}
+		String r = "";
+		for(int i = 0;s.length()>i;i++){
+			r = r + s.charAt(s.length()-i-1);
+
+		}
+
+		return r;
 	}
 }
--- src/Kadai01Test.java
+++ src/Kadai01Test.java
@@ -8,7 +8,17 @@ class Kadai01Test {
 	void testReverseString() {
 		Kadai01 k = new Kadai01();
 		assertNull(k.ReverseString(null));
-		assertEquals(4321,k.ReverseString("1234"));
+		assertEquals("1234",k.ReverseString("4321"));
+		assertEquals("12345",k.ReverseString("54321"));
+		assertEquals(" ",k.ReverseString(" "));
+		assertEquals("1あ2い3う",k.ReverseString("う3い2あ1"));
+		assertEquals("１２３４",k.ReverseString("４３２１"));
+		assertEquals("　",k.ReverseString("　"));
+
+
+
+
+
 	}
 
 }

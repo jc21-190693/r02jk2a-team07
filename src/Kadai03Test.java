58c0ac96e547ab59c4e5094fea3385ab02bba2a2
190183 <190183@e0505-26.JC-21.local>
2020-12-15 14:54:22 +0900

    aaaaaaaaaaaaaaaaaaaa

---
+++ src/Kadai03Test.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Kadai03Test {
+
+	@Test
+	void testCountString() {
+		Kadai03 k = new Kadai03();
+		String t1 = "apple";
+		assertArrayEquals(new char[] {'A','E','L','P'},k.CharacterList(t1));
+		String t2 = "ApLlE";
+		assertArrayEquals(new char[] {'A','E','L','P'},k.CharacterList(t2));
+		String t3 = null;
+		assertArrayEquals(null,k.CharacterList(t3));
+		String t4 = "  ";
+		assertArrayEquals(new char[] {},k.CharacterList(t4));
+		String t5 = "ぺっぱいなっぽーABC";
+		assertArrayEquals(new char[] {'A','B','C'},k.CharacterList(t5));
+	}
+
+}

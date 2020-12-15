b3f040a7f2982012c544077c0b27238416a99b35
190183 <190183@e0505-26.JC-21.local>
2020-12-15 14:53:12 +0900

    kadai3 kansei

--- src/Kadai03.java
+++ src/Kadai03.java
@@ -1,4 +1,3 @@
-
 public class Kadai03 {
 
 	/**
@@ -7,11 +6,38 @@ public class Kadai03 {
 	 * @param s 文字列
 	 * @return sの中に入っていた文字
 	 */
-	char[] CharacterList(String s) {
-		char[] c = new char[26];
+char[] CharacterList(String s) {
+	if(s==null) {
+		return null;
+	}
 
+	boolean[] b = new boolean[26];
+	int count = 0;
 
+	for(int i=0;i<s.length();i++) {
+		for(int n=0;n<26;n++) {
+			if(s.charAt(i)=='a'+n) {
+				b[n] = true;
+			}
+			if(s.charAt(i)=='A'+n) {
+				b[n] = true;
+			}
+		}
+	}
+	for(int a=0;a<26;a++) {
+		if(b[a]==true) {
+			count++;
+		}
+	}
 
+	char[] c = new char[count];
+	int charcount = 0;
+		for(int t=0;t<26;t++) {
+			if(b[t]==true) {
+				c[charcount] =(char) ('A'+t);
+				charcount++;
+			}
+		}
 		return c;
 	}
 }

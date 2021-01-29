7de1150a1a1ffab4d3b5084fa4c2b8ddb3a75d52
yoshzawa <yoshzawa@gmail.com>
2021-01-29 22:30:03 +0900

    全テスト実行確認済み

---
+++ src/AllTests.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	Kadai01Test.class,
+	Kadai02Test.class,
+	Kadai03Test.class
+	
+})
+public class AllTests {
+
+}
